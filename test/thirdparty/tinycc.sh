dd19ee5449123ed33d86f2156a38873d3bee9821
bruneo32 <bruneo32b@gmail.com>
2026-02-16 19:01:44 +0100

    Clean branch

--- test/thirdparty/tinycc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-repo='https://github.com/TinyCC/tinycc.git'
-. test/thirdparty/common
-git reset --hard df67d8617b7d1d03a480a28f9f901848ffbfb7ec
-
-./configure --cc=$superc
-$make clean
-$make
-$make CC=cc test
